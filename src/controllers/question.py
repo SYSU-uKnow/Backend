2ed6b0e59f28d8c9d854b0f7ca4470406d690d56
Arrkwen <919548685@qq.com>
2017-06-21 10:35:33 +0800

    推荐页面

--- src/controllers/question.py
+++ src/controllers/question.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import os
+import datetime
 import MySQLdb
-import question
+import functools
 import jieba
-from flask import Blueprint, g, jsonify, request
+from flask import Blueprint, g, jsonify, request, session
 
-visitor = Blueprint('visitor', __name__)
+question = Blueprint('question', __name__)
+
+def check_session(fun):
+  @functools.wraps(fun)
+  def wrapper(*args, **kw):
+    if 'user_id' not in session:  # æœªç™»å½•
+      return jsonify({'status':403, 'errmsg':'æœªç™»å½•'})
+    return fun(*args, **kw)
+  return wrapper
 
 def _getSomeDetail(question_id, answerer_id):
   c = g.db.cursor()
@@ -25,13 +34,12 @@ def _getSomeDetail(question_id, answerer_id):
     tmp['answerer_status'] = None
     tmp['answerer_description'] = None
 
-
-  #ÊÕÌıÈËÊı
+  #æ”¶å¬äººæ•°
   sql = 'select count(*) from listening l where l.qid = %d' % question_id
   c.execute(sql)
   listeningNum = c.fetchone()[0]
   
-  #µãÔŞÈËÊı
+  #ç‚¹èµäººæ•°
   sql = '''select count(*) from comment c where c.qid = %d and c.liked = 1''' % question_id
   c.execute(sql)
   praiseNum = c.fetchone()[0]
@@ -40,3 +48,52 @@ def _getSomeDetail(question_id, answerer_id):
   tmp['praiseNum'] = praiseNum
 
   return tmp
+
+# 1. æ¨èé¡µé¢
+@question.route('/api/questions/recommend')
+@check_session
+def getRecommend():
+  c = g.db.cursor()
+  data = {}
+  data["status"] = 200
+
+  id = session['user_id']
+
+  sql = '''select u.status from user u where u.id=%s''' % id
+  c.execute(sql)
+  result = c.fetchone()
+  status = result[0]
+
+  # åˆ†è¯
+  query_statement = " ".join(jieba.cut(status))
+  list = query_statement.split()
+
+  count = 0
+  data["data"] = []
+  for string in list:
+    sql = """select q.id, q.description, q.answerer_id, q.audioSeconds
+          from question q where q.audioUrl is not null and q.description like \'%%%s%%\'""" % string
+    c.execute(sql)
+    results = c.fetchall()
+    for row in results:
+      record = {'id':row[0], 'description':row[1], 'answerer_id': row[2], 'audioSeconds':row[3]}
+      tmp = _getSomeDetail(row[0], row[2])
+      record.update(tmp)
+      data["data"].append(record)
+      count = count+1
+
+  if count < 20:
+    sql = """select q.id, q.description, q.answerer_id, q.audioSeconds
+          from question q where q.audioUrl is not null"""
+    c.execute(sql)
+    results = c.fetchall()
+    for row in results:
+      record = {'id':row[0], 'description':row[1], 'answerer_id': row[2], 'audioSeconds':row[3]}
+      tmp = _getSomeDetail(row[0], row[2])
+      record.update(tmp)
+      data["data"].append(record)
+      count = count+1
+      if count > 20:
+        break
+
+  return jsonify(data)
