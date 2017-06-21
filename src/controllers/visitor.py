5380209ec9eaf608fa71f56a0ebf291da7b436b4
Arrkwen <919548685@qq.com>
2017-06-21 11:07:18 +0800

    主动推荐

--- src/controllers/visitor.py
+++ src/controllers/visitor.py
@@ -39,3 +39,68 @@ def _getSomeDetail(question_id, answerer_id):
   tmp['praiseNum'] = praiseNum
 
   return tmp
+
+# 1. 推荐页面
+@visitor.route('/api/wx/questions/recommend')
+def getRecommend():
+  c = g.db.cursor()
+  data = {}
+  data["status"] = 200
+
+  try:
+    data["data"] = []
+    sql = '''select q.id, q.description, q.answerer_id, q.audioSeconds
+            from question q where q.audioUrl is not null'''
+    c.execute(sql)
+    results = c.fetchall()
+    for row in results:
+      record = {'id':row[0], 'description':row[1], 'answerer_id': row[2], 'audioSeconds':row[3]}
+      tmp = _getSomeDetail(row[0], row[2])
+      record.update(tmp)
+      data["data"].append(record)
+
+  except:
+    del data['data']
+    data["errmsg"] = "推荐失败"
+    data["status"] = 500
+
+  return jsonify(data)
+
+# 2. 点击问题后进入详情页面
+@visitor.route('/api/wx/questions/<int:question_id>')
+def questionDetail(question_id):
+  c = g.db.cursor() 
+
+  data = {}
+  data["status"] = 200
+  try:
+    data["data"] = {}
+    sql = '''select q.description, q.askDate, q.audioUrl, q.asker_id, q.answerer_id, q.audioSeconds
+          from question q where q.id = %d''' % question_id
+
+    c.execute(sql)
+    result = c.fetchone()
+    data['data']['id'] = question_id
+    data['data']['description'] = result[0]
+    data['data']['askDate'] = str(result[1])
+    data['data']['audioUrl'] = result[2]
+    data['data']['audioSeconds'] = result[5] 
+
+    asker_id = result[3]
+    answerer_id = result[4]
+
+    sql = '''select u.username, u.avatarUrl from user u where u.id = %d''' % asker_id
+    c.execute(sql)
+    result = c.fetchone()
+    data['data']['asker_username'] = result[0]
+    data['data']['asker_avatarUrl'] = result[1]
+
+    tmp = _getSomeDetail(question_id, answerer_id)
+    data['data'].update(tmp)
+
+  except Exception as e:
+    del data['data']
+    data["errmsg"] = "操作失败"
+    data["status"] = 500
+
+  return jsonify(data)
