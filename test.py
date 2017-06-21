091a584e9a6e73d50e9f2ce08a4a2e3a36fbb484
linqiaonihao <824928207@qq.com>
2017-06-21 11:59:41 +0800

    test

---
+++ test.py
@@ -0,0 +1,8 @@
+import urllib2
+import urllib
+# POST
+# data = urllib.urlencode({})
+# request = urllib2.Request('http://localhost:5000/api/wx/questions/1013/listenings',data)
+# response = urllib2.urlopen(request)
+# file = response.read()
+# print file
